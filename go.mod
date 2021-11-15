dfca72e989101396dc12132ddb89eb8e1f7da123
Mirko Curtolo <mirko.curtolo86@gmail.com>
2021-11-15 14:14:59 +0100

    Draft of GUI

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
-module github.com/mirkokurt/simple-file-transformer
+module github.com/mirkokurt/simple-go-qt-gui
 
 go 1.17
+
+require (
+	github.com/gopherjs/gopherjs v0.0.0-20190411002643-bd77b112433e // indirect
+	github.com/therecipe/qt v0.0.0-20200904063919-c0c124a5770d // indirect
+)
--- main.go
+++ main.go
@@ -8,7 +8,81 @@ import (
 	"log"
 	"os"
 	"regexp"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
 )
+
+func main() {
+
+	var fileName string
+	var outputfileName string
+
+	widgets.NewQApplication(len(os.Args), os.Args)
+
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetWindowTitle("File transformer")
+	window.SetMinimumSize2(600, 400)
+
+	formLayout := widgets.NewQFormLayout(nil)
+
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetLayout(formLayout)
+
+	input := widgets.NewQLabel2("<Non selezionato>", nil, core.Qt__BypassWindowManagerHint)
+
+	button := widgets.NewQPushButton2("Seleziona", nil)
+	button.ConnectClicked(func(checked bool) {
+		//widgets.QMessageBox_Information(nil, "Titolo del messaggio", input.Text(), widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
+
+		var fileDialog = widgets.NewQFileDialog2(nil, "Open File...", "", "")
+		fileDialog.SetAcceptMode(widgets.QFileDialog__AcceptOpen)
+		fileDialog.SetFileMode(widgets.QFileDialog__ExistingFile)
+		var mimeTypes = []string{"text/plain"}
+		fileDialog.SetMimeTypeFilters(mimeTypes)
+
+		fileName = fileDialog.GetOpenFileName(nil, "Open File", "C:\\Users\\curtomir\\Desktop", "Text/plain (*.txt)", "", widgets.QFileDialog__Option(fileDialog.AcceptMode()))
+		fmt.Printf("file path is %s", fileName)
+		input.SetText(fileName)
+
+	})
+
+	output := widgets.NewQLabel2("<Non selezionato>", nil, core.Qt__BypassWindowManagerHint)
+
+	button2 := widgets.NewQPushButton2("Scegli file output", nil)
+	button2.ConnectClicked(func(checked bool) {
+		//widgets.QMessageBox_Information(nil, "Titolo del messaggio", input.Text(), widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
+
+		var fileDialog = widgets.NewQFileDialog2(nil, "Open File...", "", "")
+		fileDialog.SetAcceptMode(widgets.QFileDialog__AcceptOpen)
+		fileDialog.SetFileMode(widgets.QFileDialog__ExistingFile)
+		var mimeTypes = []string{"text/plain"}
+		fileDialog.SetMimeTypeFilters(mimeTypes)
+
+		outputfileName = fileDialog.GetSaveFileName(nil, "Save File", "C:\\Users\\curtomir\\Desktop", "Text/plain (*.txt)", "", widgets.QFileDialog__ShowDirsOnly)
+		fmt.Printf("file path is %s", outputfileName)
+		output.SetText(outputfileName)
+	})
+
+	buttonlabel := widgets.NewQLabel2("", nil, core.Qt__BypassWindowManagerHint)
+	button3 := widgets.NewQPushButton2("Trasforma", nil)
+	button3.ConnectClicked(func(checked bool) {
+		if fileName == "" || outputfileName == "" {
+			widgets.QMessageBox_Information(nil, "Errore", "Seleziona prima i file!", widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
+		} else {
+			transform(fileName, outputfileName)
+		}
+	})
+
+	formLayout.AddRow(input, button)
+	formLayout.AddRow(output, button2)
+	formLayout.AddRow(buttonlabel, button3)
+
+	window.SetCentralWidget(widget)
+	window.Show()
+
+	widgets.QApplication_Exec()
+}
 
 type rule struct {
 	re           string
@@ -18,7 +92,7 @@ type rule struct {
 
 var rule_sets []rule
 
-func main() {
+func transform(inputfile string, outputfile string) {
 
 	// Open the file with the rules to apply
 	fr, err := os.Open("rules.csv")
@@ -43,14 +117,14 @@ func main() {
 	}
 
 	// Open the input file
-	fi, err := os.Open("input.txt")
+	fi, err := os.Open(inputfile)
 	if err != nil {
 		fmt.Print("There has been an error!: ", err)
 	}
 	defer fi.Close()
 
 	// Open the output file
-	fo, err := os.Create("output.txt")
+	fo, err := os.Create(outputfile)
 	if err != nil {
 		panic(err)
 	}
